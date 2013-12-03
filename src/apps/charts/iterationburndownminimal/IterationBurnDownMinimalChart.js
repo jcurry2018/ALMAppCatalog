200757b20427199162dde9f6ace2f7413515b2a5
Joel Sherriff <jsherriff@rallydev.com>
2013-12-03 16:09:07 -0500

    S57435: add test for hybrid cfd chart and reconfigure the configuration

--- src/apps/charts/iterationburndownminimal/IterationBurnDownMinimalChart.js
+++ src/apps/charts/iterationburndownminimal/IterationBurnDownMinimalChart.js
@@ -7,68 +7,9 @@
             ],
 
             chartComponentConfig: {
-                xtype: "rallychart",
-                itemId: "iterationburndownminimalchart",
-                suppressClientMetrics: true, /* keeps rallychart::lookback query time from displaying in client metrics */
-
-                chartColors: ["#005eb8", "#666666", "#8dc63f" ],
-
-                chartConfig: {
-                     chart: {
-                         zoomType: 'xy',
-                         alignTicks: false,
-                         animation: false
-                     },
-                     plotOptions: { series: {animation: false}},
-                     legend: { enabled: true },
-                     title: { text: null },
-                     xAxis: {
-                         tickmarkPlacement: 'on',
-                         tickInterval: 1
-                     },
-                     yAxis: [
-                         {
-                             title: { text: null },
-                             min: 0,
-                             labels: { style: { color: "#005eb8" } }
-                         },
-                         {
-                            title: { text: null },
-                            min: 0,
-                            labels: { style: { color: "#8dc63f" } },
-                            opposite: true
-                         }
-                     ]
-                },
-                chartData: {
-                    categories: [ ],
-                    series: [
-                        {
-                            name: "To Do",
-                            type: "column",
-                            data: [  ],
-                            tooltip: { valueDecimals: 1, valueSuffix: ' Hours' }
-                        },
-                        {
-                            name: "Ideal",
-                            type: "line",
-                            dashStyle: "Solid",
-                            data: [ ],
-                            marker : {
-                                enabled : true,
-                                radius : 3
-                            },
-                            tooltip: { valueDecimals: 1, valueSuffix: ' Hours' }
-                        },
-                        {
-                            name: "Accepted",
-                            type: "column",
-                            data: [ ],
-                            yAxis: 1,
-                            tooltip: { valueDecimals: 1, valueSuffix: ' Points' }
-                        }
-                    ]
-                }
+               xtype: "rallychart",
+               itemId: "iterationburndownminimalchart",
+               suppressClientMetrics: true /* keeps rallychart::lookback query time from displaying in client metrics */
             }
         });
 }());
