d334651fe8758aaf436e4a1a00f24c37b14f3c4f
Matt Parrish and Steve Neely <mparrish@rallydev.com>
2014-01-08 16:59:09 -0700

    checking ext42 job for green light wrt auto-bumping (needs reverted after merging into master)

--- ci/checkAppCatalogLight.groovy
+++ ci/checkAppCatalogLight.groovy
@@ -1,7 +1,7 @@
 import hudson.model.Result
 import jenkins.model.Jenkins
 
-if (Jenkins.instance.getItem('master-app-catalog-continuous').lastCompletedBuild.result != Result.SUCCESS){
+if (Jenkins.instance.getItem('ext42-app-catalog-continuous').lastCompletedBuild.result != Result.SUCCESS){
   println "\n*** CAN'T AUTO-BUMP BECAUSE APP CATALOG IS RED!*** \n"
   return 1
 }
