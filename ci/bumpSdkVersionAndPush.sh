c40d1acc3a366b68475a8a6b9704b20c401ca45a
Rob West <rwest@rallydev.com>
2014-01-13 13:26:45 -0500

    Bump appsdk version even if the changes were in a previous build

--- ci/bumpSdkVersionAndPush.sh
+++ ci/bumpSdkVersionAndPush.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-if [ -n "$BUILD_HAS_CHANGES" ]; then
-  sed -i "s/appsdk-src:tgz:.*\",/appsdk-src:tgz:${APPSDK_SRC_VERSION}\",/" js_dependencies.json
+sed -i "s/appsdk-src:tgz:.*\",/appsdk-src:tgz:${APPSDK_SRC_VERSION}\",/" js_dependencies.json
 
-  git add js_dependencies.json
-  git commit -F appsdk.bump --author="${JOB_NAME} <bogus@rallydev.com>"
-  git push origin HEAD:$BRANCH
+git add js_dependencies.json
+git commit -F appsdk.bump --author="${JOB_NAME} <bogus@rallydev.com>"
+git push origin HEAD:$BRANCH
 
-  rm appsdk.bump
-else
-  exit 1
-fi
+rm appsdk.bump
