636c9df3447c660ba9072cd31fd1c0b568e324fd
Robb Lee and Neal Enssle <rlee@rallydev.com>
2014-02-03 12:21:21 -0700

    DE19697: Refactoring RPB to replace container with app.

--- src/apps/roadmapplanningboard/RoadmapPlanningBoardApp.js
+++ src/apps/roadmapplanningboard/RoadmapPlanningBoardApp.js
@@ -4,23 +4,132 @@
     Ext.define('Rally.apps.roadmapplanningboard.RoadmapPlanningBoardApp', {
         extend: 'Rally.app.App',
         requires: [
-            'Rally.apps.roadmapplanningboard.RoadmapPlanningBoardContainer'
+            'Rally.data.util.PortfolioItemHelper',
+            'Rally.ui.notify.Notifier',
+            'Rally.apps.roadmapplanningboard.DeftInjector',
+            'Rally.apps.roadmapplanningboard.PlanningGridBoard'
         ],
         cls: 'roadmapPlanningBoardApp',
         componentCls: 'app',
 
-        launch: function () {
-            var container = Ext.create('Rally.apps.roadmapplanningboard.RoadmapPlanningBoardContainer', {
-                context: this.context,
-                height: '100%',
-                cardboardPlugins: [
-                    {
-                        ptype: 'rallyfixedheadercardboard'
-                    }
-                ]
-            });
+        config: {
+            feedbackConfig: {
+                feedbackDialogConfig: {
+                    title: 'Feedback on Roadmap Planning Board',
+                    subject: 'Roadmap Planning Board',
+                    feedbackId: 'roadmapplanningboard'
+                }
+            },
 
-            this.add(container);
+            addNewConfig: {
+                ignoredRequiredFields: ['Name', 'Project', 'ScheduleState', 'State']
+            },
+
+            /**
+             * @cfg cardboardPlugins {Array}
+             * Extra plugins that should be added to the cardboard
+             */
+            cardboardPlugins: [
+                {
+                    ptype: 'rallyfixedheadercardboard'
+                }
+            ]
+        },
+
+        launch: function () {
+            Rally.apps.roadmapplanningboard.DeftInjector.init();
+
+            this.roadmapStore = Deft.Injector.resolve('roadmapStore');
+            this.timelineStore = Deft.Injector.resolve('timelineStore');
+
+            Ext.Ajax.on('requestexception', this._onRequestException, this);
+
+            this._retrievePITypes(function (records) {
+                this.types = [records[0].get('TypePath')];
+                this.typeNames = {
+                    child: {
+                        typePath: records[0].get('TypePath'),
+                        name: records[0].get('Name')
+                    }
+                };
+
+                if (records.length > 1) {
+                    this.typeNames.parent = {
+                        typePath: records[1].get('TypePath'),
+                        name: records[1].get('Name')
+                    };
+                }
+
+                this.gridboard = this.down('#gridboard');
+                var roadmapPromise = this.roadmapStore.load({requester: this, storeServiceName: "Planning"});
+                var timelinePromise = this.timelineStore.load({requester: this, storeServiceName: "Timeline"});
+
+                Deft.Promise.all([roadmapPromise, timelinePromise]).then({
+                    success: function (results) {
+                        var roadmap = results[0].records[0];
+                        var timeline = results[1].records[0];
+
+                        this._buildGridBoard(roadmap, timeline);
+
+                        if (Rally.BrowserTest) {
+                            Rally.BrowserTest.publishComponentReady(this);
+                        }
+                    },
+                    failure: function (operation) {
+                        var service = operation.storeServiceName || 'External';
+                        Rally.ui.notify.Notifier.showError({message: 'Failed to load app: ' + service + ' service data load issue'});
+                    },
+                    scope: this
+                });
+            });
+        },
+
+        _retrievePITypes: function (callback) {
+            Rally.data.util.PortfolioItemHelper.loadTypeOrDefault({
+                defaultToLowest: true,
+                loadAllTypes: true,
+                success: callback,
+                scope: this
+            });
+        },
+
+        _buildGridBoard: function (roadmap, timeline) {
+            var height = this.getHeight() || this._computePanelContentAreaHeight();
+            if (roadmap && timeline) {
+                this.add({
+                    xtype: 'roadmapplanninggridboard',
+                    context: this.getContext(),
+                    timeline: timeline,
+                    roadmap: roadmap,
+                    typeNames: this.typeNames,
+                    modelNames: this.types,
+                    cardboardPlugins: this.cardboardPlugins,
+                    height: height
+                });
+            } else if (!roadmap) {
+                Rally.ui.notify.Notifier.showError({message: 'No roadmap available'});
+            } else {
+                Rally.ui.notify.Notifier.showError({message: 'No timeline available'});
+            }
+        },
+
+        _computePanelContentAreaHeight: function () {
+            var content = Ext.getBody().down('#content');
+            if (!content && Rally.BrowserTest) {
+                return Ext.getBody().down('#testDiv').getHeight();
+            }
+            return content.getHeight() - content.down('.page').getHeight();
+        },
+
+        _onRequestException: function (connection, response, requestOptions) {
+            var requester = requestOptions.operation && requestOptions.operation.requester;
+            var el = this.getEl();
+
+            if (requester === this && el) {
+//                this.gridboard.setHeight(this._computePanelContentAreaHeight());
+                this.setHeight(this._computePanelContentAreaHeight());
+                el.mask('Roadmap planning is <strong>temporarily unavailable</strong>, please try again in a few minutes.', "roadmap-service-unavailable-error");
+            }
         }
     });
 })();
--- test/spec/roadmapplanningboard/RoadmapPlanningBoardAppSpec.coffee
+++ test/spec/roadmapplanningboard/RoadmapPlanningBoardAppSpec.coffee
@@ -2,13 +2,14 @@ Ext = window.Ext4 || window.Ext
 
 Ext.require [
   'Rally.apps.roadmapplanningboard.RoadmapPlanningBoardApp',
-  'Rally.test.apps.roadmapplanningboard.helper.TestDependencyHelper'
+  'Rally.test.apps.roadmapplanningboard.helper.TestDependencyHelper',
+  'Rally.test.mock.ModelObjectMother'
 ]
 
 describe 'Rally.apps.roadmapplanningboard.RoadmapPlanningBoardApp', ->
   helpers
-    createApp: (settings = {}, context = {}) ->
-      @app = Ext.create 'Rally.apps.roadmapplanningboard.RoadmapPlanningBoardApp',
+    createApp: (expectError = false, config = {}) ->
+      config = _.extend
         context: Ext.create 'Rally.app.Context',
           initialValues:
             Ext.merge
@@ -19,15 +20,26 @@ describe 'Rally.apps.roadmapplanningboard.RoadmapPlanningBoardApp', ->
               workspace:
                 WorkspaceConfiguration:
                   DragDropRankingEnabled: true
-            , context
+            , {}
 
-        settings: settings
+        settings: {}
         renderTo: 'testDiv'
+      ,config
 
-      @waitForComponentReady(@app.down('container'))
+      @app = Ext.create 'Rally.apps.roadmapplanningboard.RoadmapPlanningBoardApp', config
+
+      if expectError
+        @once
+          condition: => @errorNotifyStub.calledOnce
+      else
+        @waitForComponentReady(@app).then =>
+          @planningBoard = @app.down 'roadmapplanningboard'
 
   beforeEach ->
     Rally.test.apps.roadmapplanningboard.helper.TestDependencyHelper.loadDependencies()
+    @timelineStore = Deft.Injector.resolve('timelineStore')
+    @roadmapStore = Deft.Injector.resolve('roadmapStore')
+    @errorNotifyStub = @stub Rally.ui.notify.Notifier, 'showError'
     @ajax.whenQuerying('TypeDefinition').respondWith Rally.test.mock.data.WsapiModelFactory.getModelDefinition('PortfolioItemFeature')
     @ajax.whenQuerying('PortfolioItem/Feature').respondWith []
 
@@ -35,10 +47,71 @@ describe 'Rally.apps.roadmapplanningboard.RoadmapPlanningBoardApp', ->
     @app?.destroy()
     Deft.Injector.reset()
 
-  it 'should create a container', ->
+  it 'should use the provided context', ->
     @createApp().then =>
-      expect(@app.down('container')).toBeDefined()
+      expect(@app.getContext().getProject().ObjectID).toBe 123456
 
-  it 'should forward app context to container', ->
+  it 'should render a planning board with a timeline', ->
     @createApp().then =>
-      expect(@app.down('container').getContext().getProject().ObjectID).toBe 123456
+      expect(@planningBoard.timeline.getId()).toBe @timelineStore.first().getId()
+
+  it 'should render a planning board with a roadmap', ->
+    @createApp().then =>
+      expect(@planningBoard.timeline.getId()).toBe @timelineStore.first().getId()
+
+  it 'should define height for planning board based on content window', ->
+    Ext.DomHelper.append Ext.getBody(), '<div id="content" style="height: 600px;"><div class="page" style="height: 20px;"></div></div>'
+    @createApp().then =>
+      expect(@planningBoard.getHeight()).toBe 580
+
+  it 'should define height for grid board based on container height', ->
+    @createApp(false, {height: 600}).then =>
+      expect(@app.getHeight()).toBe 600
+
+  it 'should notify of error if the timeline store fails to load', ->
+    @stub @timelineStore, 'load', ->
+      deferred = new Deft.promise.Deferred()
+      deferred.reject({storeServiceName: 'Timeline'});
+      deferred.promise
+
+    @createApp(true).then =>
+      expect(@errorNotifyStub.lastCall.args[0]).toEqual
+        message: 'Failed to load app: Timeline service data load issue'
+
+  it 'should notify of error if the roadmap store fails to load', ->
+    @stub @roadmapStore, 'load', ->
+      deferred = new Deft.promise.Deferred()
+      deferred.reject({storeServiceName: 'Planning'});
+      deferred.promise
+
+    @createApp(true).then =>
+      expect(@errorNotifyStub.lastCall.args[0]).toEqual
+        message: 'Failed to load app: Planning service data load issue'
+
+  it 'should notify of error if there is no roadmap available', ->
+    @stub @roadmapStore, 'load', ->
+      deferred = new Deft.promise.Deferred()
+      deferred.resolve { records: {} }
+      deferred.promise
+
+    @createApp(true).then =>
+      expect(@errorNotifyStub.lastCall.args[0]).toEqual
+        message: 'No roadmap available'
+
+  it 'should notify of error if there is no timeline available', ->
+    @stub @timelineStore, 'load', ->
+      deferred = new Deft.promise.Deferred()
+      deferred.resolve { records: {} }
+      deferred.promise
+
+    @createApp(true).then =>
+      expect(@errorNotifyStub.lastCall.args[0]).toEqual
+        message: 'No timeline available'
+
+  describe 'Service error handling', ->
+    it 'should display a friendly notification if any service (planning, timeline, WSAPI) is unavailable', ->
+      @createApp().then =>
+        Ext.Ajax.fireEvent('requestexception', null, null, { operation:
+          requester: @app })
+
+        expect(@app.getEl().getHTML()).toContain 'temporarily unavailable'
