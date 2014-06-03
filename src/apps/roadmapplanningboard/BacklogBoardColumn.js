79da048afc97433374614c176f15925ee3b8c05d
Katie Thompson, Andrew Grauch and Wade Peterson <kthompson@rallydev.com>
2014-06-03 12:44:41 -0600

    S67176: search backlog on rpb

--- src/apps/roadmapplanningboard/BacklogBoardColumn.js
+++ src/apps/roadmapplanningboard/BacklogBoardColumn.js
@@ -5,6 +5,8 @@
         extend: 'Rally.apps.roadmapplanningboard.PlanningBoardColumn',
         alias: 'widget.backlogplanningcolumn',
 
+        requires: ['Rally.ui.SearchField'],
+
         config: {
             /**
              * @cfg {Rally.data.Store}
@@ -22,12 +24,27 @@
             }
         },
 
+        drawHeader: function () {
+            this.callParent(arguments);
+
+            this.getColumnHeader().add(
+                Ext.create('Rally.ui.SearchField', {
+                    searchOnKeyup: true,
+                    showSearchButton: true,
+                    listeners: {
+                        search: this._searchBacklog,
+                        scope: this
+                    }
+                })
+            );
+        },
+
         getColumnIdentifier: function () {
             return "roadmapplanningboard.backlog.column";
         },
 
         _getAllPlanFeatures: function () {
-            return _.reduce (this.planStore.data.items, function (result, plan) {
+            return _.reduce(this.planStore.data.items, function (result, plan) {
                 return result.concat(plan.get('features'));
             }, []);
         },
@@ -36,6 +53,16 @@
             var recordId = featureRecord.get('_refObjectUUID'),
                 found = _.find(this._getAllPlanFeatures(), { id: recordId });
             return !found;
+        },
+
+        _searchBacklog: function (cmp, value) {
+            if (this.storeConfig.search !== value) {
+                this.refresh({
+                    storeConfig: {
+                        search: value ? Ext.String.trim(value) : ""
+                    }
+                });
+            }
         }
     });
 
