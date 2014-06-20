001b5b056f56ce92b6d2a6ef8868de3f79b00e88
Eli Soman and Dave Thompson <esoman@rallydev.com>
2014-06-20 15:13:20 -0600

    S68395: enable compact=true to work with shallowFetch=true.

--- src/apps/iterationtrackingboard/IterationTrackingBoardApp.js
+++ src/apps/iterationtrackingboard/IterationTrackingBoardApp.js
@@ -113,7 +113,8 @@
                     root: {expanded: true},
                     enableHierarchy: true,
                     pageSize: this.getGridPageSizes()[1],
-                    childPageSizeEnabled: context.isFeatureEnabled('EXPAND_ALL_TREE_GRID_CHILDREN')
+                    childPageSizeEnabled: context.isFeatureEnabled('EXPAND_ALL_TREE_GRID_CHILDREN'),
+                    useShallowFetch: context.isFeatureEnabled('COMPACT_WSAPI_REQUESTS') ? false : true
                 };
 
             if(!context.isFeatureEnabled('USE_CUSTOM_FILTER_POPOVER_ON_ITERATION_TRACKING_APP')) {
@@ -153,7 +154,7 @@
                 cardBoardConfig: this._getBoardConfig(),
                 gridConfig: this._getGridConfig(gridStore),
                 storeConfig: {
-                    useShallowFetch: true,
+                    useShallowFetch: context.isFeatureEnabled('COMPACT_WSAPI_REQUESTS') ? false : true,
                     filters: [context.getTimeboxScope().getQueryFilter()]
                 },
                 addNewPluginConfig: {
