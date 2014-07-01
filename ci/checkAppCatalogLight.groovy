cb37ee7fc01362c098057b27e4cb0e5aa806d6c1
Mike Rampson <mrampson@rallydev.com>
2014-07-01 17:05:38 -0600

    Revert "update check app catalog light to use local build info"
    This reverts commit b14754aec6ecbf31171e058298f6b6175851b904.

--- ci/checkAppCatalogLight.groovy
+++ ci/checkAppCatalogLight.groovy
@@ -4,7 +4,7 @@ import jenkins.model.Jenkins
 def check_light = build.buildVariables["CHECK_LIGHT"]
 if (check_light == "false") {
     print "\n* Not checking the App Catalog light! *\n\n"
-} else if (build.project.lastCompletedBuild.result != Result.SUCCESS) {
+} else if (Jenkins.instance.getItem('master-app-catalog-continuous').lastCompletedBuild.result != Result.SUCCESS) {
     print "\n*** CAN'T AUTO-BUMP BECAUSE APP CATALOG IS RED! ***\n\n"
     return 1
 }
