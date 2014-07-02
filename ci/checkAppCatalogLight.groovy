7658d7e553b1c18f39f23fb77b8b3aaa2cc41983
Mike Rampson <mrampson@rallydev.com>
2014-07-02 10:20:46 -0600

    S69214: Resolve job via job_name parameter rather than hard-coded value

--- ci/checkAppCatalogLight.groovy
+++ ci/checkAppCatalogLight.groovy
@@ -1,10 +1,17 @@
 import hudson.model.Result
 import jenkins.model.Jenkins
 
+def jobName = build.buildVariables["DOWNSTREAM_JOB_NAME"]
+def job = Jenkins.instance.getItem(jobName)
+if (!job) {
+    println "\n*** CAN'T FIND JOB ${jobName}! ***\n\n"
+    return 1
+}
+
 def check_light = build.buildVariables["CHECK_LIGHT"]
 if (check_light == "false") {
     print "\n* Not checking the App Catalog light! *\n\n"
-} else if (Jenkins.instance.getItem('master-app-catalog-continuous').lastCompletedBuild.result != Result.SUCCESS) {
+} else if (job.lastCompletedBuild?.result != Result.SUCCESS) {
     print "\n*** CAN'T AUTO-BUMP BECAUSE APP CATALOG IS RED! ***\n\n"
     return 1
 }
