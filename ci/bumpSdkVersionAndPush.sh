3ac670a37e8e879d97bc1bbce9500694d5dc9afe
jlivesay <jlivesay@rallydev.com>
2014-07-04 13:42:51 -0600

    Adding git.config information to bumpSdkVersionAndPush.sh script until this gets added to the base docker build image.

--- ci/bumpSdkVersionAndPush.sh
+++ ci/bumpSdkVersionAndPush.sh
@@ -3,6 +3,8 @@
 sed -i "s/appsdk-src:tgz:.*\",/appsdk-src:tgz:${APPSDK_SRC_VERSION}\",/" js_dependencies.json
 
 git add js_dependencies.json
+git config user.name Hudson
+git config.user.email hudson@rallydev.com
 git commit -F appsdk.bump --author="${JOB_NAME} <bogus@rallydev.com>"
 
 if [ $SHOULD_PUSH == "true" ]; then
