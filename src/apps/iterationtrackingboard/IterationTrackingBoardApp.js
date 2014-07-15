38ecf6044108610e6aed95a5a01a998531757a2e
Alan Brockett <abrockett@rallydev.com>
2014-07-15 13:46:52 -0600

    DE12777: Re-blacklist TestCase for CustomFilterPopover

--- src/apps/iterationtrackingboard/IterationTrackingBoardApp.js
+++ src/apps/iterationtrackingboard/IterationTrackingBoardApp.js
@@ -239,6 +239,7 @@
                                 'DragAndDropRank',
                                 'Iteration',
                                 'PortfolioItem',
+                                'TestCase',
                                 'TestCaseResult',
                                 'VersionId'
                             ],
