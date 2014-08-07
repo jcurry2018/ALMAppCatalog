1b67b76c37e1f5e0726e5f9bd7eb921dcc1a00c1
Dave Thompson, Matt Novinger and Mike Holm <dthompson@rallydev.com>
2014-08-07 15:10:36 -0600

    Added GridBoard and artifact type chooser to TreeGridApp.

--- src/apps/treegrid/TreeGridApp.js
+++ src/apps/treegrid/TreeGridApp.js
@@ -5,51 +5,108 @@
         extend: 'Rally.app.App',
         requires: [
           'Rally.ui.grid.TreeGrid',
-          'Rally.ui.grid.plugin.TreeGridExpandedRowPersistence'
+          'Rally.ui.grid.plugin.TreeGridExpandedRowPersistence',
+          'Rally.ui.gridboard.GridBoard',
+          'Rally.ui.picker.PillPicker',
+          'Rally.ui.picker.MultiObjectPicker'
         ],
         alias: 'widget.treegridapp',
         componentCls: 'treegrid',
 
-//        modelNames: ['User Story'],// 'Defect', 'Defect Suite', 'Test Set'],
-        modelNames: ['PortfolioItem/Initiative'],
-
-        columnNames: ['Name', 'Owner', 'Project'],
-
-        storeConfig: {},
+        config: {
+            defaultSettings: {
+                modelNames: ['PortfolioItem/Feature'],
+                columnNames: ['Name', 'Owner', 'Project']
+            }
+        },
 
         launch: function () {
-            var plugins = [],
-                context = this.getContext();
-
-            if (context.isFeatureEnabled('EXPAND_ALL_TREE_GRID_CHILDREN')) {
-                plugins.push({
-                    ptype: 'rallytreegridexpandedrowpersistence',
-                    enableExpandLoadingMask: !context.isFeatureEnabled('EXPAND_ALL_LOADING_MASK_DISABLE')
-                });
-            }
-
             this._getGridStore().then({
                 success: function(gridStore) {
-                    this.add({
-                        xtype: 'rallytreegrid',
-                        plugins: plugins,
-                        store: gridStore,
-                        columnCfgs: this.columnNames,
-                        stateId: this.getContext().getScopedStateId('custom-treegrid'),
-                        stateful: true
-                    });
-
-                    gridStore.load();
+                    this._addGridBoard(gridStore);
                 },
                 scope: this
             });
         },
 
-        _getGridStore: function() {
+        getSettingsFields: function() {
+            var artifactQuery = '((((((TypePath = "hierarchicalrequirement") OR (TypePath = "defect")) OR (TypePath = "defectsuite")) OR (TypePath = "testset")) OR (TypePath = "testcase")) OR (Parent.TypePath = "portfolioitem"))';
+
+            return [{
+                xtype: 'rallypillpicker',
+                showPills: true,
+                name: 'modelNames',
+                comboBoxCfg: {
+                    xtype: 'rallymultiobjectpicker',
+                    name: 'modelNames',
+                    fieldLabel: 'Objects',
+                    labelWidth: 75,
+                    labelSeparator: '',
+                    width: 400,
+                    remoteFilter: false,
+                    modelType: 'TypeDefinition',
+                    selectionKey: 'TypePath',
+                    storeLoadOptions: {
+                        params: {
+                            order: 'Name ASC',
+                            fetch: 'Name,TypePath',
+                            query: artifactQuery
+                        }
+                    }
+                }
+            }];
+        },
+
+        _addGridBoard: function(gridStore) {
             var context = this.getContext(),
-                storeConfig = Ext.apply(this.storeConfig, {
-                    models: this.modelNames,
-                    autoLoad: false,
+                stateString = 'custom-treegrid',
+                stateId = context.getScopedStateId(stateString);
+
+            this.add({
+                itemId: 'gridBoard',
+                xtype: 'rallygridboard',
+                stateId: 'iterationtracking-gridboard',
+                context: context,
+                plugins: [],
+                toggleState: 'grid',
+                modelNames: this.modelNames,
+                cardBoardConfig: {},
+                gridConfig: this._getGridConfig(gridStore, context, stateId),
+                storeConfig: {},
+                height: this.getHeight()
+            });
+        },
+
+        _getGridConfig: function(gridStore, context, stateId) {
+            var gridConfig = {
+                xtype: 'rallytreegrid',
+                store: gridStore,
+                columnCfgs: this.getSetting('columnNames') || this.columnNames,
+                summaryColumns: [],
+                enableBulkEdit: false,
+                plugins: [],
+                stateId: stateId,
+                stateful: true
+            };
+
+            if (context.isFeatureEnabled('EXPAND_ALL_TREE_GRID_CHILDREN')) {
+                gridConfig.plugins.push({
+                    ptype: 'rallytreegridexpandedrowpersistence',
+                    enableExpandLoadingMask: !context.isFeatureEnabled('EXPAND_ALL_LOADING_MASK_DISABLE')
+                });
+            }
+
+            return gridConfig;
+        },
+
+        _getGridStore: function() {
+            var modelNames = this.getSetting('modelNames') || this.modelNames;
+            modelNames = _.isString(modelNames) ? modelNames.split(',') : modelNames;
+
+            var context = this.getContext(),
+                storeConfig = Ext.apply(this.storeConfig || {}, {
+                    models: modelNames,
+                    autoLoad: true,
                     remoteSort: true,
                     root: {expanded: true},
                     pageSize: 200,
--- test/spec/treegrid/TreeGridAppSpec.coffee
+++ test/spec/treegrid/TreeGridAppSpec.coffee
@@ -9,33 +9,40 @@ describe 'Rally.apps.treegrid.TreeGridApp', ->
   helpers
     getTreeGridAppConfig: (featureEnabled) ->
       modelNames: ['PortfolioItem/Project']
+      getHeight: -> 250
       getContext: ->
         get: ->
         isFeatureEnabled: -> featureEnabled
         getScopedStateId: -> 'someStateId'
 
   beforeEach ->
-    @piQueryStub = @ajax.whenQuerying('artifact').respondWith()
+    @ajax.whenQuerying('artifact').respondWith()
 
   afterEach ->
-    if (@treeGridApp)
-      @treeGridApp.destroy()
+    _.invoke Ext.ComponentQuery.query('treegridapp'), 'destroy'
 
   it 'should initialize', ->
-    @treeGridApp = Ext.create 'Rally.apps.treegrid.TreeGridApp', @getTreeGridAppConfig(false)
-    expect(Ext.isDefined(@treeGridApp)).toBeTruthy()
+    treeGridApp = Ext.create 'Rally.apps.treegrid.TreeGridApp', @getTreeGridAppConfig(false)
+    expect(Ext.isDefined(treeGridApp)).toBeTruthy()
 
   it 'should use the row expansion plugin', ->
-    @treeGridApp = Ext.create 'Rally.apps.treegrid.TreeGridApp', @getTreeGridAppConfig(true)
-    treeGrid = @treeGridApp.down 'rallytreegrid'
+    treeGridApp = Ext.create 'Rally.apps.treegrid.TreeGridApp', @getTreeGridAppConfig(true)
+    expect(_.find(treeGridApp.down('#gridBoard').gridConfig.plugins, ptype: 'rallytreegridexpandedrowpersistence')).toBeTruthy()
 
-    expect(_.filter(treeGrid.plugins, ptype: 'rallytreegridexpandedrowpersistence').length).toBe 1
+  it 'should accept model strings', ->
+    appCfg = _.extend @getTreeGridAppConfig(true),
+      defaultSettings:
+        modelNames: 'hierarchicalrequirement,defect'
 
-  it 'should fetch configured column attributes', ->
-    @treeGridApp = Ext.create 'Rally.apps.treegrid.TreeGridApp', @getTreeGridAppConfig(true)
-    @waitForCallback(@piQueryStub).then =>
-      fetchedColumns = @piQueryStub.getCall(0).args[0].params.fetch.split(',')
-      _.each(@treeGridApp.columnNames, (columnName) ->
-        expect(fetchedColumns).toContain columnName
-      )
+    treeGridApp = Ext.create 'Rally.apps.treegrid.TreeGridApp', appCfg
+    parentTypes = treeGridApp.down('#gridBoard').gridConfig.store.parentTypes
+    expect(parentTypes.length).toBe 2
+    expect(parentTypes).toContainAll ['hierarchicalrequirement','defect']
 
+  it 'should show type picker in settings', ->
+    treeGridApp = Ext.create 'Rally.apps.treegrid.TreeGridApp', @getTreeGridAppConfig(true)
+
+    settings = treeGridApp.getSettingsFields()
+    expect(settings[0].xtype).toBe 'rallypillpicker'
+    expect(settings[0].comboBoxCfg.fieldLabel).toBe 'Objects'
+    expect(settings[0].comboBoxCfg.modelType).toBe 'TypeDefinition'
