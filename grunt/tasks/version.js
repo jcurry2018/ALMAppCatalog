6260b4e9eb6f30a0905884d525750fea590254fd
Doug Alcouffe and Will Beard <dalcouffe@rallydev.com>
2014-08-20 11:13:58 -0600

    Issue 16: Added artifactPrefix

--- grunt/tasks/version.js
+++ grunt/tasks/version.js
@@ -20,8 +20,9 @@ module.exports = function(grunt) {
     };
 
     var getBuildVersion = function(revision) {
+        var artifactPrefix = process.env.ARTIFACT_PREFIX ? process.env.ARTIFACT_PREFIX + '-' : '';
         var counter = process.env.PIPELINE_COUNTER || process.env.BUILD_NUMBER || 'dev';
-        return "" + counter + "-" + (revision.substr(0, 7)) + '-sdk-' + getSdkVersion();
+        return "" + artifactPrefix + counter + "-" + (revision.substr(0, 7)) + '-sdk-' + getSdkVersion();
     };
 
     var version = function() {
