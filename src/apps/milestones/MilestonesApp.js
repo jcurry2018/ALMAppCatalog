ddee876fc2e568726de42de9257ac6f9da71607f
Kyle Morse <kmorse@rallydev.com>
2014-10-19 21:25:08 -0600

    Revert "S74448: [Milestones] Updated Plan->Milestones page to treegrid"
    This reverts commit dc57258ddf0f1a9bd9d964478fd0ebb784833f0a.

--- src/apps/milestones/MilestonesApp.js
+++ src/apps/milestones/MilestonesApp.js
@@ -1,69 +1,95 @@
-(function () {
+(function() {
     var Ext = window.Ext4 || window.Ext;
 
     Ext.define('Rally.apps.milestones.MilestonesApp', {
-        extend: 'Rally.apps.common.GridBoardApp',
+        extend: 'Rally.app.App',
         requires: [
             'Rally.ui.DateField',
             'Rally.ui.combobox.MilestoneProjectComboBox',
-            'Rally.ui.grid.MilestoneProjectEditor'
+            'Rally.ui.grid.MilestoneProjectEditor',
+            'Rally.ui.grid.RowActionColumn',
+            'Rally.ui.menu.item.DeleteMenuItem'
         ],
         cls: 'milestones-app',
-        config: {
-            enableOwnerFilter: false,
-            modelNames: ['Milestone'],
-            defaultSettings: {
-                columnNames: ['FormattedID','DisplayColor','Name','TargetDate','TotalArtifactCount','TargetProject']
-            }
-        },
 
-        getStateId: function () {
-            return 'milestone';
-        },
-
-        getGridConfig: function () {
-            return _.merge(this.callParent(arguments), {
-                enableRanking: false,
-                noDataItemName: 'milestone',
-                rowActionColumnConfig: {
-                    rowActionsFn: function (record) {
-                        return Rally.ui.grid.MilestoneProjectEditor.shouldDisableEditing(record.get('TargetProject')) ? [] : [
-                            {
-                                xtype: 'rallyrecordmenuitemdelete',
-                                record: record
+        launch: function() {
+            var context = this.getContext();
+            this.add(Ext.create('Rally.ui.list.ListView', {
+                model: Ext.identityFn('milestone'),
+                addNewConfig: this._addNewConfig(),
+                gridConfig: {
+                    columnCfgs: [
+                        {
+                            xtype: 'rallyrowactioncolumn',
+                            rowActionsFn: function (record) {
+                                return [
+                                    {
+                                        xtype: 'rallyrecordmenuitemdelete',
+                                        record: record
+                                    }
+                                ];
                             }
-                        ];
-                    }
+                        },
+                        {
+                            dataIndex: 'FormattedID',
+                            renderer: function(formattedID, obj, record) {
+                                return Ext.create('Rally.ui.renderer.template.FormattedIDTemplate').apply({
+                                   _type: 'milestone',
+                                   FormattedID: formattedID,
+                                   DisplayColor: record.get('DisplayColor'),
+                                   Recycled: true
+                                });
+                            }
+                        },
+                        {
+                            dataIndex: 'DisplayColor',
+                            width: 60,
+                            text: 'Color'
+                        },
+                        'Name',
+                        'TargetDate',
+                        {
+                            dataIndex: 'TotalArtifactCount',
+                            text: 'Item Count',
+                            tdCls: 'artifacts'
+                        },
+                        {
+                            dataIndex: 'TargetProject',
+                            isCellEditable: function(project){
+                                return !Rally.ui.grid.MilestoneProjectEditor.shouldDisableEditing(project);
+                            },
+                            renderer: function(project) {
+                                if (project === ''){
+                                    return '<div class="permission-required">Project Permissions Required</div>';
+                                }
+                                if (project === null) {
+                                    return 'All projects in ' +  context.getWorkspace().Name;
+                                }
+                                return project.Name;
+                            },
+                            text: 'Project'
+                        }
+                    ],
+                    enableRanking: true,
+                    storeConfig: {
+                        fetch: 'FormattedID,Name,TargetDate,Artifacts,TargetProject,TotalArtifactCount',
+                        sorters: [{
+                            property: 'TargetDate',
+                            direction: 'DESC'
+                        }]
+                    },
+                    showRowActionsColumn: false,
+                    viewConfig: {
+                        emptyText: this._getEmptyText()
+                    },
+                    showIcon: false
                 }
-            });
+            }));
         },
 
-        getAdditionalFetchFields: function () {
-            return ['DisplayColor'];
-        },
-
-        getGridStoreConfig: function () {
-            return {
-                enableHierarchy: false,
-                childPageSizeEnabled: false
-            };
-        },
-
-        getFieldPickerConfig: function () {
-            var config = this.callParent(arguments);
-            config.gridFieldBlackList = _.union(config.gridFieldBlackList, [
-                'Artifacts',
-                'CreationDate',
-                'Projects',
-                'VersionId'
-            ]);
-            return _.merge(config, {
-                gridAlwaysSelectedValues: ['FormattedID', 'Name', 'TargetDate', 'TotalArtifactCount', 'TargetProject']
-            });
-        },
-
-        getAddNewConfig: function () {
-            return {
+        _addNewConfig: function() {
+            return this.context.getPermissions().hasEditorAccessToAnyProject() ? {
+                recordTypes: ['Milestone'],
                 showAddWithDetails: false,
                 openEditorAfterAddFailure: false,
                 minWidth: 800,
@@ -80,7 +106,11 @@
                         value: Rally.util.Ref.getRelativeUri(this.getContext().getProject())
                     }
                 ]
-            };
+            } : {};
+        },
+
+        _getEmptyText: function() {
+            return '<div class="no-data-container"><div class="primary-message">Looks like no milestones have been created.</div><div class="secondary-message">Add a milestone with the Add New button above.</div></div>';
         }
     });
 })();
--- test/spec/portfolioitemstreegrid/PortfolioItemsTreeGridAppSpec.coffee
+++ test/spec/portfolioitemstreegrid/PortfolioItemsTreeGridAppSpec.coffee
@@ -23,13 +23,39 @@ describe 'Rally.apps.portfolioitemstreegrid.PortfolioItemsTreeGridApp', ->
         context: context
         renderTo: 'testDiv'
 
+
     renderApp: ->
       @app = @createApp()
       @waitForComponentReady @app
 
   beforeEach ->
-    @piHelper = new Helpers.PortfolioItemGridBoardHelper @
-    @piHelper.stubPortfolioItemRequests()
+    Rally.environment.getContext().context.subscription.Modules = ['Rally Portfolio Manager']
+
+    @theme = Rally.test.mock.data.WsapiModelFactory.getModelDefinition('PortfolioItemTheme')
+    @initiative = Rally.test.mock.data.WsapiModelFactory.getModelDefinition('PortfolioItemInitiative')
+    @feature = Rally.test.mock.data.WsapiModelFactory.getModelDefinition('PortfolioItemFeature')
+
+    @piTypes = [
+      @theme
+      @initiative
+      @feature
+    ]
+
+    @typeRequest = @ajax.whenQuerying('typedefinition').respondWith @piTypes
+
+    @stub Rally.data.util.PortfolioItemTypeDefList::, 'getArray', =>
+      Deft.Promise.when @piTypes
+
+    @stub Rally.data.TypeDefinitionFetcher::, 'fetchTypeDefinitions', =>
+      Deft.Promise.when Results: @piTypes
+
+    theme = @mom.getRecord('PortfolioItem/Theme')
+    @ajax.whenQuerying('PortfolioItem/Theme ').respondWith [theme.data]
+    initiative = @mom.getRecord('PortfolioItem/Initiative')
+    @ajax.whenQuerying('PortfolioItem/Initiative').respondWith [initiative.data]
+    feature = @mom.getRecord('PortfolioItem/Feature')
+    @ajax.whenQuerying('PortfolioItem/Feature').respondWith [feature.data]
+    @ajax.whenQuerying('artifact').respondWith()
 
   afterEach ->
     _.invoke Ext.ComponentQuery.query('portfoliotemstreegridapp'), 'destroy'
@@ -40,8 +66,8 @@ describe 'Rally.apps.portfolioitemstreegrid.PortfolioItemsTreeGridApp', ->
 
   it 'should use the row expansion plugin', ->
     @renderApp().then =>
-      plugins = _.map @app.gridboard.gridConfig.plugins, (plugin) -> plugin.ptype || plugin
-      expect(plugins).toContain 'rallytreegridexpandedrowpersistence'
+      plugins = @app.gridboard.gridConfig.plugins
+      expect(_.find(plugins, ptype: 'rallytreegridexpandedrowpersistence')).toBeTruthy()
 
   it 'should configure the tree store to the portfolio item types', ->
     @renderApp().then =>
