ad98782b367d64c01179d09950551130975793f9
Wade Peterson and Komran Rashidov <wpeterson@rallydev.com>
2014-10-20 11:23:34 -0600

    Revert "Revert "Revert "Revert Stefaning"S74448: [Milestones] Updated Plan->Milestones page to treegrid""""
    This reverts commit 6e70c94cfce6ca6f764911fb26686f9ed76bd33f.

--- src/apps/milestones/MilestonesApp.js
+++ src/apps/milestones/MilestonesApp.js
@@ -1,95 +1,69 @@
-(function() {
+(function () {
     var Ext = window.Ext4 || window.Ext;
 
     Ext.define('Rally.apps.milestones.MilestonesApp', {
-        extend: 'Rally.app.App',
+        extend: 'Rally.apps.common.GridBoardApp',
         requires: [
             'Rally.ui.DateField',
             'Rally.ui.combobox.MilestoneProjectComboBox',
-            'Rally.ui.grid.MilestoneProjectEditor',
-            'Rally.ui.grid.RowActionColumn',
-            'Rally.ui.menu.item.DeleteMenuItem'
+            'Rally.ui.grid.MilestoneProjectEditor'
         ],
         cls: 'milestones-app',
-
-        launch: function() {
-            var context = this.getContext();
-            this.add(Ext.create('Rally.ui.list.ListView', {
-                model: Ext.identityFn('milestone'),
-                addNewConfig: this._addNewConfig(),
-                gridConfig: {
-                    columnCfgs: [
-                        {
-                            xtype: 'rallyrowactioncolumn',
-                            rowActionsFn: function (record) {
-                                return [
-                                    {
-                                        xtype: 'rallyrecordmenuitemdelete',
-                                        record: record
-                                    }
-                                ];
-                            }
-                        },
-                        {
-                            dataIndex: 'FormattedID',
-                            renderer: function(formattedID, obj, record) {
-                                return Ext.create('Rally.ui.renderer.template.FormattedIDTemplate').apply({
-                                   _type: 'milestone',
-                                   FormattedID: formattedID,
-                                   DisplayColor: record.get('DisplayColor'),
-                                   Recycled: true
-                                });
-                            }
-                        },
-                        {
-                            dataIndex: 'DisplayColor',
-                            width: 60,
-                            text: 'Color'
-                        },
-                        'Name',
-                        'TargetDate',
-                        {
-                            dataIndex: 'TotalArtifactCount',
-                            text: 'Item Count',
-                            tdCls: 'artifacts'
-                        },
-                        {
-                            dataIndex: 'TargetProject',
-                            isCellEditable: function(project){
-                                return !Rally.ui.grid.MilestoneProjectEditor.shouldDisableEditing(project);
-                            },
-                            renderer: function(project) {
-                                if (project === ''){
-                                    return '<div class="permission-required">Project Permissions Required</div>';
-                                }
-                                if (project === null) {
-                                    return 'All projects in ' +  context.getWorkspace().Name;
-                                }
-                                return project.Name;
-                            },
-                            text: 'Project'
-                        }
-                    ],
-                    enableRanking: true,
-                    storeConfig: {
-                        fetch: 'FormattedID,Name,TargetDate,Artifacts,TargetProject,TotalArtifactCount',
-                        sorters: [{
-                            property: 'TargetDate',
-                            direction: 'DESC'
-                        }]
-                    },
-                    showRowActionsColumn: false,
-                    viewConfig: {
-                        emptyText: this._getEmptyText()
-                    },
-                    showIcon: false
-                }
-            }));
+        config: {
+            enableOwnerFilter: false,
+            modelNames: ['Milestone'],
+            defaultSettings: {
+                columnNames: ['FormattedID','DisplayColor','Name','TargetDate','TotalArtifactCount','TargetProject']
+            }
         },
 
-        _addNewConfig: function() {
-            return this.context.getPermissions().hasEditorAccessToAnyProject() ? {
-                recordTypes: ['Milestone'],
+        getStateId: function () {
+            return 'milestone';
+        },
+
+        getGridConfig: function () {
+            return _.merge(this.callParent(arguments), {
+                enableRanking: false,
+                noDataItemName: 'milestone',
+                rowActionColumnConfig: {
+                    rowActionsFn: function (record) {
+                        return Rally.ui.grid.MilestoneProjectEditor.shouldDisableEditing(record.get('TargetProject')) ? [] : [
+                            {
+                                xtype: 'rallyrecordmenuitemdelete',
+                                record: record
+                            }
+                        ];
+                    }
+                }
+            });
+        },
+
+        getAdditionalFetchFields: function () {
+            return ['DisplayColor'];
+        },
+
+        getGridStoreConfig: function () {
+            return {
+                enableHierarchy: false,
+                childPageSizeEnabled: false
+            };
+        },
+
+        getFieldPickerConfig: function () {
+            var config = this.callParent(arguments);
+            config.gridFieldBlackList = _.union(config.gridFieldBlackList, [
+                'Artifacts',
+                'CreationDate',
+                'Projects',
+                'VersionId'
+            ]);
+            return _.merge(config, {
+                gridAlwaysSelectedValues: ['FormattedID', 'Name', 'TargetDate', 'TotalArtifactCount', 'TargetProject']
+            });
+        },
+
+        getAddNewConfig: function () {
+            return {
                 showAddWithDetails: false,
                 openEditorAfterAddFailure: false,
                 minWidth: 800,
@@ -106,11 +80,7 @@
                         value: Rally.util.Ref.getRelativeUri(this.getContext().getProject())
                     }
                 ]
-            } : {};
-        },
-
-        _getEmptyText: function() {
-            return '<div class="no-data-container"><div class="primary-message">Looks like no milestones have been created.</div><div class="secondary-message">Add a milestone with the Add New button above.</div></div>';
+            };
         }
     });
 })();
