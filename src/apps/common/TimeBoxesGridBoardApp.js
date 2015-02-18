572b0ba9099cc0248a57ed250e527df08a7fa455
Garston Tremblay and Rick Lansky <gtremblay@rallydev.com>
2015-02-18 10:58:35 -0700

    S81701: Update Releases page to Ext-based tech

---
+++ src/apps/common/TimeBoxesGridBoardApp.js
@@ -0,0 +1,59 @@
+(function () {
+    var Ext = window.Ext4 || window.Ext;
+
+    Ext.define('Rally.apps.common.TimeBoxesGridBoardApp', {
+        extend: 'Rally.app.GridBoardApp',
+        requires: ['Rally.ui.DateField'],
+
+        enableXmlExport: true,
+
+        getAddNewConfig: function () {
+            return Ext.apply(this.callParent(arguments), {
+                additionalFields: [
+                    {
+                        xtype: 'rallydatefield',
+                        allowBlank: false,
+                        emptyText: this.startDateEmptyText,
+                        name: this.startDateFieldName,
+                        paramName: 'startDate'
+                    }, {
+                        xtype: 'rallydatefield',
+                        allowBlank: false,
+                        emptyText: this.endDateEmptyText,
+                        name: this.endDateFieldName,
+                        paramName: 'endDate'
+                    }
+                ],
+                ignoredRequiredFields: ['GrossEstimateConversionRatio', 'Name', 'Project', 'State', this.startDateFieldName, this.endDateFieldName],
+                listeners: {
+                    beforecreate: this._onBeforeCreate
+                },
+                minWidth: 800,
+                openEditorAfterAddFailure: false,
+                showAddWithDetails: true,
+                showRank: false
+            });
+        },
+
+        getFilterControlConfig: function () {
+            return {
+                blackListFields: ['GrossEstimateConversionRatio', 'Theme']
+            };
+        },
+
+        getGridConfig: function () {
+            return _.merge(this.callParent(arguments), {
+                enableSummaryRow: false,
+                rowActionColumnConfig: {
+                    menuOptions: {
+                        showInlineAdd: false
+                    }
+                }
+            });
+        },
+
+        _onBeforeCreate: function(addNew, record) {
+            record.set('State', 'Planning');
+        }
+    });
+})();
--- src/apps/releases/ReleasesApp.js
+++ src/apps/releases/ReleasesApp.js
@@ -2,40 +2,14 @@
     var Ext = window.Ext4 || window.Ext;
 
     Ext.define('Rally.apps.releases.ReleasesApp', {
-        extend: 'Rally.app.GridBoardApp',
-        cls: 'releases-app',
-        enableOwnerFilter: false,
-        enableRanking: false,
+        extend: 'Rally.apps.common.TimeBoxesGridBoardApp',
+
         modelNames: ['Release'],
-        columnNames: ['Name','Theme','ReleaseStartDate','ReleaseDate','PlannedVelocity','State'],
-        statePrefix: 'releases',
+        statePrefix: 'release',
 
-        getAddNewConfig: function () {
-            return {
-                showRank: false,
-                showAddWithDetails: true,
-                openEditorAfterAddFailure: true,
-                ignoredRequiredFields: ['Name', 'State', 'Project', 'GrossEstimateConversionRatio'],
-                minWidth: 800,
-                additionalFields: [
-                    {
-                        xtype: 'rallydatefield',
-                        emptyText: 'Select Start Date',
-                        name: 'ReleaseStartDate'
-                    },{
-                        xtype: 'rallydatefield',
-                        emptyText: 'Select Release Date',
-                        name: 'ReleaseDate'
-                    }
-                ],
-                listeners: {
-                    beforecreate: this._onBeforeCreate
-                }
-            };
-        },
-
-        _onBeforeCreate: function(addNew, record, params) {
-            record.set('State', 'Planning');
-        }
+        startDateEmptyText: 'Select Start Date',
+        startDateFieldName: 'ReleaseStartDate',
+        endDateEmptyText: 'Select Release Date',
+        endDateFieldName: 'ReleaseDate'
     });
 })();
