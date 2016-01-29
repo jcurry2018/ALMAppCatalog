b733541ea35fece83bb560c02f7443b6f9ff7cec
Erika Brice and Robert Everett <ebrice@rallydev.com>
2016-01-29 11:03:02 -0700

    S108171: Add updated filtering and saved views to the Backlog

--- src/apps/backlog/BacklogApp.js
+++ src/apps/backlog/BacklogApp.js
@@ -5,9 +5,23 @@
         extend: 'Rally.app.GridBoardApp',
         alias: 'widget.backlogapp',
         columnNames: ['FormattedID', 'Name', 'PlanEstimate', 'Priority', 'Owner'],
-        requires: ['Rally.data.wsapi.Filter'],
+        requires: [
+            'Rally.data.Ranker',
+            'Rally.data.wsapi.Filter',
+            'Rally.ui.gridboard.plugin.GridBoardInlineFilterControl',
+            'Rally.ui.gridboard.plugin.GridBoardSharedViewControl'
+        ],
         modelNames: ['hierarchicalrequirement', 'defect', 'defectsuite'],
         statePrefix: 'backlog',
+
+        getAddNewConfig: function () {
+            var config = {};
+            if (this.getContext().isFeatureEnabled('F8943_UPGRADE_TO_NEWEST_FILTERING_SHARED_VIEWS_ON_MANY_PAGES')) {
+                config.margin = 0;
+            }
+
+            return _.merge(this.callParent(arguments), config);
+        },
 
         getPermanentFilters: function (types) {
             types = (types === undefined ? ['hierarchicalrequirement', 'defect', 'defectSuite'] : types);
@@ -54,14 +68,51 @@
             };
         },
 
-        getGridBoardCustomFilterControlConfig: function() {
+        getGridBoardCustomFilterControlConfig: function () {
+            var context = this.getContext();
+            var blackListFields = ['Iteration', 'PortfolioItem', 'Release'];
+            var whiteListFields = ['Milestones', 'Tags'];
+
+            if (context.isFeatureEnabled('F8943_UPGRADE_TO_NEWEST_FILTERING_SHARED_VIEWS_ON_MANY_PAGES')) {
+                return {
+                    ptype: 'rallygridboardinlinefiltercontrol',
+                    inlineFilterButtonConfig: {
+                        stateful: true,
+                        stateId: context.getScopedStateId('backlog-inline-filter'),
+                        filterChildren: true,
+                        modelNames: this.modelNames,
+                        inlineFilterPanelConfig: {
+                            quickFilterPanelConfig: {
+                                defaultFields: [
+                                    'ArtifactSearch',
+                                    'Owner',
+                                    'ModelType'
+                                ],
+                                addQuickFilterConfig: {
+                                    blackListFields: blackListFields,
+                                    whiteListFields: whiteListFields
+                                }
+                            },
+                            advancedFilterPanelConfig: {
+                                advancedFilterRowsConfig: {
+                                    propertyFieldConfig: {
+                                        blackListFields: blackListFields,
+                                        whiteListFields: whiteListFields
+                                    }
+                                }
+                            }
+                        }
+                    }
+                };
+            }
+
             return {
                 showOwnerFilter: false,
                 showIdFilter: true,
-                idFilterConfig:{
+                idFilterConfig: {
                     stateful: true,
                     stateId: this.getScopedStateId('backlog-id-filter'),
-                    storeConfig:{
+                    storeConfig: {
                         autoLoad: true,
                         pageSize: 25,
                         fetch: ['FormattedID', '_refObjectName'],
@@ -69,6 +120,62 @@
                     }
                 }
             };
+        },
+
+        getSharedViewConfig: function() {
+            var context = this.getContext();
+            if (context.isFeatureEnabled('F8943_UPGRADE_TO_NEWEST_FILTERING_SHARED_VIEWS_ON_MANY_PAGES')) {
+                return {
+                    ptype: 'rallygridboardsharedviewcontrol',
+                    sharedViewConfig: {
+                        stateful: true,
+                        stateId: context.getScopedStateId('backlog-shared-view'),
+                        defaultViews: _.map(this._getDefaultViews(), function(view) {
+                            Ext.apply(view, {
+                                Value: Ext.JSON.encode(view.Value, true)
+                            });
+                            return view;
+                        }, this),
+                        enableUrlSharing: this.isFullPageApp !== false
+                    },
+                    enableGridEditing: context.isFeatureEnabled('S91174_ISP_SHARED_VIEWS_MAKE_PREFERENCE_NAMES_UPDATABLE')
+                };
+            }
+
+            return {};
+        },
+
+        _getDefaultViews: function() {
+            var rankColumnDataIndex = this.getContext().getWorkspace().WorkspaceConfiguration.DragDropRankingEnabled ? Rally.data.Ranker.RANK_FIELDS.DND : Rally.data.Ranker.RANK_FIELDS.MANUAL;
+
+            return [
+                {
+                    Name: 'Default View',
+                    identifier: 1,
+                    Value: {
+                        toggleState: 'grid',
+                        columns: _.flatten([
+                            { dataIndex: rankColumnDataIndex },
+                            _.map(this.columnNames, function(columnName) {
+                                return { dataIndex: columnName }
+                            })
+                        ]),
+                        sorters:[{ property: rankColumnDataIndex, direction: 'ASC' }]
+                    }
+                }
+            ];
+        },
+
+        getGridBoardConfig: function () {
+            var config = this.callParent(arguments);
+            return _.merge(config, {
+                listeners: {
+                    viewchange: function() {
+                        this.loadGridBoard();
+                    },
+                    scope: this
+                }
+            });
         },
 
         _getModelFor: function(type) {
--- test/spec/backlog/BacklogAppSpec.coffee
+++ test/spec/backlog/BacklogAppSpec.coffee
@@ -27,6 +27,11 @@ describe 'Rally.apps.backlog.BacklogApp', ->
         , context)
       )
 
+    stubFeatureToggle: (toggles, value = true) ->
+      stub = @stub(Rally.app.Context.prototype, 'isFeatureEnabled');
+      stub.withArgs(toggle).returns(value) for toggle in toggles
+      stub
+
   beforeEach ->
     @ajax.whenQuerying('artifact').respondWith(@mom.getData('hierarchicalrequirement', count: 5))
 
@@ -91,3 +96,53 @@ describe 'Rally.apps.backlog.BacklogApp', ->
 
     it 'should return filters for user stories and defect suites', ->
       @expectFiltersForTypes ['hierarchicalrequirement', 'defectSuite']
+
+  describe 'filtering panel plugin', ->
+    helpers
+      getPlugin: (filterptype='rallygridboardinlinefiltercontrol') ->
+        gridBoard = @app.down 'rallygridboard'
+        _.find gridBoard.plugins, (plugin) ->
+          plugin.ptype == filterptype
+
+    it 'should have the old filter component by default', ->
+      @createApp().then =>
+        expect(@getPlugin('rallygridboardcustomfiltercontrol')).toBeDefined()
+
+    it 'should use rallygridboard filtering plugin', ->
+      @stubFeatureToggle ['F8943_UPGRADE_TO_NEWEST_FILTERING_SHARED_VIEWS_ON_MANY_PAGES'], true
+      @createApp().then =>
+        expect(@getPlugin()).toBeDefined()
+
+  describe 'shared view plugin', ->
+    helpers
+      getPlugin: (filterptype='rallygridboardsharedviewcontrol') ->
+        gridBoard = @app.down 'rallygridboard'
+        _.find gridBoard.plugins, (plugin) ->
+          plugin.ptype == filterptype
+
+    it 'should not have shared view plugin if the toggle is off', ->
+      @createApp().then =>
+        expect(@getPlugin()).not.toBeDefined()
+
+    it 'should use rallygridboard shared view plugin if toggled on', ->
+      @stubFeatureToggle ['F8943_UPGRADE_TO_NEWEST_FILTERING_SHARED_VIEWS_ON_MANY_PAGES'], true
+      @createApp().then =>
+        plugin = @getPlugin()
+        expect(plugin).toBeDefined()
+        expect(plugin.sharedViewConfig.stateful).toBe true
+        expect(plugin.sharedViewConfig.stateId).toBe @app.getContext().getScopedStateId('backlog-shared-view')
+
+    it 'sets current view on viewchange', ->
+      @stubFeatureToggle ['F8943_UPGRADE_TO_NEWEST_FILTERING_SHARED_VIEWS_ON_MANY_PAGES'], true
+      @createApp().then =>
+        loadSpy = @spy(@app, 'loadGridBoard')
+        @app.gridboard.fireEvent 'viewchange'
+        expect(loadSpy).toHaveBeenCalledOnce()
+        expect(@app.down('#gridBoard')).toBeDefined()
+
+    it 'contains default view', ->
+      @stubFeatureToggle ['F8943_UPGRADE_TO_NEWEST_FILTERING_SHARED_VIEWS_ON_MANY_PAGES'], true
+      @createApp().then =>
+        plugin = @getPlugin()
+        expect(plugin.controlCmp.defaultViews.length).toBe 1
+        expect(plugin.controlCmp.defaultViews[0].Name).toBe 'Default View'
