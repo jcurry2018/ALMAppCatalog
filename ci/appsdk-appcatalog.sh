7de2886ca33f1b47a105826f42247ba215a57ea0
Mike Rampson and Will Klein <mrampson@rallydev.com>
2016-03-01 11:44:51 -0700

    S110168: add shell script to handle running ci build

---
+++ ci/appsdk-appcatalog.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -el
+
+browser=$1
+npm install
+grunt clean
+
+if [ ! -z "$APPSDK_SRC_VERSION" ]; then
+    npm install rally-appsdk@${APPSDK_SRC_VERSION}
+fi
+
+grunt test:${browser}:faster --maxSpecRunners=4
