20e1f38f0ebb83d46b9117e824660eda395d28e9
Mike Rampson and Will Klein <mrampson@rallydev.com>
2016-03-03 19:59:04 -0700

    S110168:add npm specific bump scripts

---
+++ ci/generateAppSdkBumpCommitMsgWithNpm.groovy
@@ -0,0 +1,48 @@
+import jenkins.model.Jenkins
+
+def appSdkSrcVersion = build.buildVariables.APPSDK_SRC_VERSION
+def env = build.characteristicEnvVars
+
+def packageJson = new hudson.FilePath(build.workspace, 'package.json').readToString()
+def prevNpmVersionMatcher = packageJson =~ /\"rally-appsdk": \"([^"]+)\"/
+def prevNpmVersion = prevNpmVersionMatcher[0][1]
+
+println "Previous NPM Version: ${prevNpmVersion}"
+
+def versionMap = [:]
+def p = "npm dist-tag ls rally-appsdk".execute()
+p.text.splitEachLine("\n",{
+  it.each { x ->
+    y = x.split(": ")
+    versionMap.put(y[1],y[0])
+  }
+})
+
+def prevSdkBuildNumberMatcher = versionMap[prevNpmVersion] =~ /-(\d+)-/
+def prevSdkBuildNumber = prevSdkBuildNumberMatcher[0][1] as int
+println "Previous SDK Build Number: ${prevSdkBuildNumber}"
+
+def buildName = build.buildVariables.UPSTREAM_JOB_NAME ?: 'bridge/appsdk-alm-bridge'
+def upstreamProject = Jenkins.instance.getItemByFullName(buildName)
+def newBuildNumberMatcher = appSdkSrcVersion =~ /-(\d+)-/
+def newBuildNumber = newBuildNumberMatcher[0][1] as int
+println "New SDK Build Number: ${newBuildNumber}"
+
+StringBuilder commitMsg = new StringBuilder() << "${env.JOB_NAME} ${env.BUILD_NUMBER} bumping sdk to ${appSdkSrcVersion}" << "\n\n"
+
+if(prevSdkBuildNumber < newBuildNumber){
+    (prevSdkBuildNumber + 1..newBuildNumber).each { buildNumber ->
+ 	    def buildInList = upstreamProject.getBuildByNumber(buildNumber)
+
+        if(buildInList) {
+            buildInList.changeSet.each { change ->
+                commitMsg << "sdk commit: RallySoftware/appsdk@${change.commitId}\n"
+                commitMsg << "author: ${change.authorName}\n"
+                commitMsg << "message: ${change.msg}\n\n"
+            }
+        }
+    }
+}
+
+def appsdkBump = new hudson.FilePath(build.workspace, 'appsdk.bump')
+appsdkBump.write(commitMsg as String, null)
