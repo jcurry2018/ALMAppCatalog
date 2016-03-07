510147f9c6e46aa36d98948630dd9a4a5a647218
Will Klein and Mike Rampson <wklein@rallydev.com>
2016-03-07 15:24:58 -0700

    S109526: add npm dist-tags for app-catalog version lookup

--- ci/publishAppCatalogArtifact.sh
+++ ci/publishAppCatalogArtifact.sh
@@ -6,3 +6,7 @@ grunt nexus:verify
 grunt writeVersion
 echo -e "APP_CATALOG_SRC_VERSION=$(cat appcatalog.version)" > appcatalog.version
 grunt npm:publish
+
+echo "Npm tag: ${ARTIFACT_PREFIX}-${ARTIFACT_VERSION}"
+bump_version=`npm ll --depth=0 rally-app-catalog | grep rally-app-catalog | awk -F'@' '{print $2}'`
+npm dist-tag add rally-app-catalog@${bump_version} "${ARTIFACT_PREFIX}-${ARTIFACT_VERSION}"
